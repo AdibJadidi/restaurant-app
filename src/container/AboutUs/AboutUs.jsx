a15071916d94b4e721f13546daf856c24b64f567
adoool <adibjadidi74@gmail.com>
2022-02-10 16:03:26 +0330

    third commit

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="aboutus"
   >
-    <div className="app_aboutus-overlay flex__center">
+    <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
 
